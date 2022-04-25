3f1896fdb0c15408e32fc75c59140b29c0ecfb21
Bas van der Heijden <d.s.vanderheijden@tudelft.nl>
2022-04-25 18:52:07 +0200

    fix: refactor `is_reactive` to `sync`.

--- eagerx_pybullet/bridge.py
+++ eagerx_pybullet/bridge.py
@@ -31,7 +31,7 @@ class PybulletBridge(Bridge):
         spec: BridgeSpec,
         rate: float,
         process: Optional[int] = process.NEW_PROCESS,
-        is_reactive: Optional[bool] = True,
+        sync: Optional[bool] = True,
         real_time_factor: Optional[float] = 0,
         simulate_delays: Optional[bool] = True,
         log_level: Optional[int] = ERROR,
@@ -47,7 +47,7 @@ class PybulletBridge(Bridge):
         :param spec: Holds the desired configuration in a Spec object.
         :param rate: Rate of the bridge
         :param process: {0: NEW_PROCESS, 1: ENVIRONMENT, 2: BRIDGE, 3: EXTERNAL}
-        :param is_reactive: Run reactive or async
+        :param sync: Run reactive or async
         :param real_time_factor: Simulation speed. 0 == "as fast as possible".
         :param simulate_delays: Boolean flag to simulate delays.
         :param log_level: {0: SILENT, 10: DEBUG, 20: INFO, 30: WARN, 40: ERROR, 50: FATAL}
@@ -75,7 +75,7 @@ class PybulletBridge(Bridge):
         # Modify default bridge params
         spec.config.rate = rate
         spec.config.process = process
-        spec.config.is_reactive = is_reactive
+        spec.config.sync = sync
         spec.config.real_time_factor = real_time_factor
         spec.config.simulate_delays = simulate_delays
         spec.config.log_level = log_level
