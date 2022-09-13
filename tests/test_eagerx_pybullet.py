99e7b1012f20ab628c8acb605568168eed5bd73e
Bas van der Heijden <d.s.vanderheijden@tudelft.nl>
2022-09-13 17:41:47 +0200

    chore:rm cam, cube, extra sensors

--- tests/test_eagerx_pybullet.py
+++ tests/test_eagerx_pybullet.py
@@ -26,27 +26,29 @@ def test_eagerx_pybullet(control_mode, p):
     from example.objects.camera.objects import Camera
     import example.objects
     urdf = os.path.dirname(example.objects.__file__) + "/camera/assets/realsense2_d435.urdf"
-    cam = Camera.make(
-        "cam",
-        rate=rate,
-        sensors=["rgb", "rgba", "rgbd"],
-        urdf=urdf,
-        optical_link="camera_color_optical_frame",
-        calibration_link="camera_bottom_screw_frame",
-    )
-    graph.add(cam)
+    # cam = Camera.make(
+    #     "cam",
+    #     rate=rate,
+    #     sensors=["rgb", "rgba", "rgbd"],
+    #     urdf=urdf,
+    #     optical_link="camera_color_optical_frame",
+    #     calibration_link="camera_bottom_screw_frame",
+    # )
+    # graph.add(cam)
 
     # Create solid object
-    from example.objects.solid.objects import Solid
-    cube = Solid.make("cube", urdf="cube_small.urdf", rate=rate)
-    graph.add(cube)
+    # from example.objects.solid.objects import Solid
+    # cube = Solid.make("cube", urdf="cube_small.urdf", rate=rate)
+    # graph.add(cube)
 
     # Create arm
     from example.objects.vx300s.objects import Vx300s
     arm = Vx300s.make(
         "viper",
-        sensors=["pos", "vel", "ft", "at"],
-        actuators=["joint_control", "gripper_control"],
+        # sensors=["pos", "vel", "ft", "at"],
+        sensors=["pos"],
+        # actuators=["joint_control", "gripper_control"],
+        actuators=["joint_control"],
         states=["pos", "vel", "gripper"],
         rate=rate,
         control_mode=control_mode,
@@ -55,11 +57,11 @@ def test_eagerx_pybullet(control_mode, p):
 
     # Connect the nodes
     graph.connect(action="joints", target=arm.actuators.joint_control)
-    graph.connect(action="gripper", target=arm.actuators.gripper_control)
+    # graph.connect(action="gripper", target=arm.actuators.gripper_control)
     graph.connect(source=arm.sensors.pos, observation="observation")
-    graph.connect(source=arm.sensors.vel, observation="vel")
-    graph.connect(source=arm.sensors.ft, observation="ft")
-    graph.connect(source=arm.sensors.at, observation="at")
+    # graph.connect(source=arm.sensors.vel, observation="vel")
+    # graph.connect(source=arm.sensors.ft, observation="ft")
+    # graph.connect(source=arm.sensors.at, observation="at")
 
     # Define engines
     from eagerx_pybullet.engine import PybulletEngine
