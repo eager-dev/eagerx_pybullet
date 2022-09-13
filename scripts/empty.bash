1819c8f3943a3d164d6cfd9617d629c649cc9da1
Bas van der Heijden <d.s.vanderheijden@tudelft.nl>
2022-09-13 18:08:16 +0200

    chore: create dirs

--- scripts/empty.bash
+++ scripts/empty.bash
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 
-if [ -s diff.txt ]; then
+if [ -s /opt/ros/noetic/setup.bash ]; then
         # The file is not-empty.
         echo "/opt/ros/noetic/setup.bash exists!"
 else
         # The file is empty.
+        mkdir -p /opt/ros/noetic/
         touch /opt/ros/noetic/setup.bash
 fi
